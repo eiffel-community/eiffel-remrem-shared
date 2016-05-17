3ca0dfafa933824a76d6b64d1edad538f532d84b
Hongkai Liu <hongkailiu@users.noreply.github.com>
2016-05-17 14:19:13 -0400

    use JsonObject for event nodes

--- src/main/java/com/ericsson/eiffel/remrem/message/services/MsgService.java
+++ src/main/java/com/ericsson/eiffel/remrem/message/services/MsgService.java
@@ -3,8 +3,6 @@ package com.ericsson.eiffel.remrem.message.services;
 
 import com.google.gson.JsonObject;
 
-import java.util.Map;
-
 public interface MsgService {
-    String generateMsg(String msgType, JsonObject msgNodes, Map<String, String> eventParams);
+    String generateMsg(String msgType, JsonObject msgNodes, JsonObject eventNodes);
 }
