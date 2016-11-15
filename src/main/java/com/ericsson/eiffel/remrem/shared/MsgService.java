be42748172e179b997d920e8d94b59b9edd2cc23
erjamni <raja.maragani@ericsson.com>
2016-11-15 16:14:03 +0100

    Shared extended for the eventid

--- src/main/java/com/ericsson/eiffel/remrem/shared/MsgService.java
+++ src/main/java/com/ericsson/eiffel/remrem/shared/MsgService.java
@@ -4,5 +4,12 @@ package com.ericsson.eiffel.remrem.shared;
 import com.google.gson.JsonObject;
 
 public interface MsgService {
+    
+    String EVENT_ID = "eventId";
+    String ID = "id";
+    String META = "meta";
+    String EIFFEL_MESSAGE_VERSIONS = "eiffelMessageVersions";
+    
     String generateMsg(String msgType, JsonObject bodyJson);
+    String getEventId(JsonObject bodyJson);
 }
