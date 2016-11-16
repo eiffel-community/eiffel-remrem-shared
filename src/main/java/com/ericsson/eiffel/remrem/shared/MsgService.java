a2b8196ac9e8c34a4ebf14b5ee313d831a9f996f
Raja Maragani <raja.maragani@tcs.com>
2016-11-16 11:12:59 +0100

    Shared extended for the eventid (#6)
    * Shared extended for the eventid
    
    * Added java doc comments on methods
    
    * Removed constants and improved documentation
    
    * improved documentation

--- src/main/java/com/ericsson/eiffel/remrem/shared/MsgService.java
+++ src/main/java/com/ericsson/eiffel/remrem/shared/MsgService.java
@@ -1,8 +1,21 @@
 package com.ericsson.eiffel.remrem.shared;
 
-
 import com.google.gson.JsonObject;
 
 public interface MsgService {
+
+    /**
+     * Generates the event as string from the input json Object based on the message type.
+     * @param String msgType
+     * @param JsonObject bodyJson
+     * @return the generated event as string format
+     */
     String generateMsg(String msgType, JsonObject bodyJson);
+
+    /**
+     * Returns the Event Id from the json object. 
+     * @param JsonObject bodyJson
+     * @return the eventId from json object if event id not available then returns the null value
+     */
+    String getEventId(JsonObject bodyJson);
 }
