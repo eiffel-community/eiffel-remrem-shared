50ac447d46e9cb32869c085d940302f911d224e9
erjamni <raja.maragani@ericsson.com>
2016-11-16 09:58:12 +0100

    Added java doc comments on methods

--- src/main/java/com/ericsson/eiffel/remrem/shared/MsgService.java
+++ src/main/java/com/ericsson/eiffel/remrem/shared/MsgService.java
@@ -1,15 +1,26 @@
 package com.ericsson.eiffel.remrem.shared;
 
-
 import com.google.gson.JsonObject;
 
 public interface MsgService {
-    
+
     String EVENT_ID = "eventId";
     String ID = "id";
     String META = "meta";
     String EIFFEL_MESSAGE_VERSIONS = "eiffelMessageVersions";
-    
+
+    /**
+     * 
+     * @param msgType
+     * @param bodyJson
+     * @return
+     */
     String generateMsg(String msgType, JsonObject bodyJson);
+
+    /**
+     * Get the event id from json object.
+     * @param bodyJson
+     * @return
+     */
     String getEventId(JsonObject bodyJson);
 }
